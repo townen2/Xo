3f6b08a217782a0dd5cef20c3508e5ea90fb36b1
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-24 16:51:24 -0400

    Update main-uptime.js

--- plugins/main-uptime.js
+++ plugins/main-uptime.js
@@ -6,55 +6,46 @@ const FormData = require("form-data");
 const { cmd } = require("../command");
 
 function formatRemainingTime(ms) {
-  let totalSeconds = Math.floor(ms / 1000);
-  let days = Math.floor(totalSeconds / (3600 * 24));
-  let hours = Math.floor((totalSeconds % (3600 * 24)) / 3600);
-  let minutes = Math.floor((totalSeconds % 3600) / 60);
-  let seconds = totalSeconds % 60;
+let totalSeconds = Math.floor(ms / 1000);
+let days = Math.floor(totalSeconds / (3600 * 24));
+let hours = Math.floor((totalSeconds % (3600 * 24)) / 3600);
+let minutes = Math.floor((totalSeconds % 3600) / 60);
+let seconds = totalSeconds % 60;
 
-  return `*┃❍ ${days} ᴅᴀʏ(s)*\n*┃❍ ${hours} ʜᴏᴜʀ(s)*\n*┃❍ ${minutes} ᴍɪɴᴜᴛᴇ(s)*\n*┃❍ ${seconds} sᴇᴄᴏɴᴅ(s)*`;
+return *┃❍ ${days} ᴅᴀʏ(s)*\n*┃❍ ${hours} ʜᴏᴜʀ(s)*\n*┃❍ ${minutes} ᴍɪɴᴜᴛᴇ(s)*\n*┃❍ ${seconds} sᴇᴄᴏɴᴅ(s)*;
 }
 
-// Newsletter channel info
-
 cmd({
-  pattern: "runtime",
-  alias: ["uptime", "run"],
-  react: "⏳",
-  desc: "Show bot alive status and uptime",
-  category: "system",
-  filename: __filename
+pattern: "runtime",
+alias: ["uptime", "run"],
+react: "⏳",
+desc: "Show bot alive status and uptime",
+category: "system",
+filename: __filename
 }, async (client, message, args, { reply }) => {
-  try {
-    const uptimeMs = process.uptime() * 1000;
-    const uptimeFormatted = formatRemainingTime(uptimeMs);
+try {
+const start = Date.now();
+const uptimeMs = process.uptime() * 1000;
+const uptimeFormatted = formatRemainingTime(uptimeMs);
 
-    const status = `
-*𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃 𝐈𝐒 𝐑𝐔𝐍𝐍𝐈𝐍𝐆!!*
-*𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃 𝐔𝐏𝐓𝐈𝐌𝐄 𝐈𝐍𝐅𝐎:*
+const status = `
 
-*╭═════════════════⊷*
+𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃 𝐈𝐒 𝐑𝐔𝐍𝐍𝐈𝐍𝐆!!
+𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃 𝐔𝐏𝐓𝐈𝐌𝐄 𝐈𝐍𝐅𝐎:
+
+╭═════════════════⊷
 ${uptimeFormatted}
-*╰═════════════════⊷*
-    `.trim();
+╰═════════════════⊷
+`;
 
-    await conn.sendMessage(from, {
-    image: { url: 'https://files.catbox.moe/roubzi.jpg' },
-    caption: status,
-    contextInfo: {
-        mentionedJid: [m.sender],
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363401051937059@newsletter', // ou ton JID actuel
-            newsletterName: '𝐌𝐄𝐆𝐀𝐋𝐎𝐃𝐎𝐍-𝐌𝐃',
-            serverMessageId: 143
-        }
-    }
-}, { quoted: mek })
+await client.sendMessage(message.chat, {  
+  image: { url: "https://files.catbox.moe/roubzi.jpg" },  
+  caption: status.trim(),  
+}, { quoted: message });
 
-  } catch (err) {
-    console.error("Alive Command Error:", err);
-    await reply(`❌ Error: ${err.message || err}`);
-  }
+} catch (err) {
+console.error("Alive Command Error:", err);
+await reply(❌ Error: ${err.message || err});
+}
 });
+

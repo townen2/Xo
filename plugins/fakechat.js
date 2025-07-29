e29a15c968604fb4f44a8a97e15803023574da07
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-28 20:45:41 -0400

    Update fakechat.js

--- plugins/fakechat.js
+++ plugins/fakechat.js
@@ -4,27 +4,20 @@ cmd({
   pattern: "iphonequote",
   alias: ["fakechat", "iphone"],
   category: "tools",
-  react: "😉",
+  react: "📱",
   desc: "Generate a fake iPhone quoted message",
   use: "<text>",
   filename: __filename
 },
 async (conn, m, { q, prefix, command }) => {
-  try {
-    if (!q) {
-      return m.reply(`❌ Example:\n.iphone I'm the president`);
-    }
-
-    const url = `https://veloria-ui.vercel.app/imagecreator/fake-chat?messageText=${encodeURIComponent(q)}`;
-    console.log("Generated image URL:", url);
-
-    await conn.sendMessage(m.chat, {
-      image: { url, mimetype: 'image/jpeg' },
-      caption: `📱 *Fake iPhone quoted message*`
-    }, { quoted: m });
-
-  } catch (err) {
-    console.error(err);
-    m.reply("❌ Failed to generate image. Error: " + err.message);
+  if (!q) {
+    return m.reply(`❌ Example:\n${prefix + command} Life is beautiful`);
   }
+
+  const url = `https://veloria-ui.vercel.app/imagecreator/fake-chat?time=12:00&messageText=${encodeURIComponent(q)}&batteryPercentage=100`;
+
+  await conn.sendMessage(m.chat, {
+    image: { url },
+    caption: "📱 *Fake iPhone Quoted Message*"
+  }, { quoted: m });
 });

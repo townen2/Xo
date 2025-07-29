f3d2338a069ebb3d671a5e32c23334292732538c
Frfjhfghv <margarettedessources42@gmail.com>
2025-07-28 22:02:22 -0400

    Update rw-tool.js

--- plugins/rw-tool.js
+++ plugins/rw-tool.js
@@ -6,7 +6,7 @@ cmd({
   alias: ["randomwall", "wallpaper"],
   react: "🌌",
   desc: "Download random wallpapers based on keywords.",
-  category: "wallpapers",
+  category: "search",
   use: ".rw <keyword>",
   filename: __filename
 }, async (conn, m, store, { from, args, reply }) => {
